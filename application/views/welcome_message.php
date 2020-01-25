5c8bff2918e75126d6ca05e6e6f36310fe1666e5
codingcyber <vivek@codingcyber.com>
2020-01-25 21:18:19 +0530

    2-2 Setting Up the Initial Configuration

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -75,7 +75,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
-
+                <a href="<?php echo base_url('welcome'); ?>">Click Here</a>
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
 
