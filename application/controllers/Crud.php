fa4994e45d311d308d36672cd1dd625b9f3edd0f
codingcyber <vivek@codingcyber.com>
2020-02-05 14:35:14 +0530

    5-8 Updating Data with CI Query Builder Class

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,8 +9,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Crud extends CI_Controller {
+
     // create operation
-    function index(){
+    function index() {
         //echo "crud index controller";
         //$data['age'] = 20;
         $data['ages'] = array(20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
@@ -19,121 +20,132 @@ class Crud extends CI_Controller {
         $this->load->view('crud/index', $data);
         $this->load->view('crud/templates/footer');
     }
-    
+
     // update operation
-    function edit($id){
+    function edit($id) {
         //echo "crud edit controller : ".$id;
         $data['id'] = $id;
         // based on this id, we will get the records from the database. Pass it to the edit view - model method
-        
+
         $this->load->model('crud_model');
         $userData = $this->crud_model->getRecord($id);
         $this->load->view('crud/templates/header');
         $this->load->view('crud/edit', $userData);
         $this->load->view('crud/templates/footer');
     }
-    
+
     // create view controller & views - after trying watch this video
     // read operation
-    function view(){
+    function view() {
         //we will get the user information from model method - we will with sample data
-        
+
         $this->load->model('crud_model');
         $user = $this->crud_model->getAll();
-        
+
         // Controller - View - Model : MVC
-        
+
         $this->load->view('crud/templates/header');
         $this->load->view('crud/view', $user);
         $this->load->view('crud/templates/footer');
     }
-    
+
     // delete operation
-    function delete($id){
+    function delete($id) {
         echo "delete controller method : " . $id;
         // we will pass this to model method for deleting the record.
     }
-	
-	// test the database connecion
-	
-	function databasetest(){
-		$this->load->model('crud_model');
-		$this->crud_model->dbtest();
-	}
-	
-	// Query method to CRUD INSERT/SELECT/UPDATE/DELETE
 
-	function viewdbtest(){
-		$this->load->model('dbtest_model');
-		$data['users'] = $this->dbtest_model->queryselect();
-		
-		$this->load->view('crud/templates/header');
+    // test the database connecion
+
+    function databasetest() {
+        $this->load->model('crud_model');
+        $this->crud_model->dbtest();
+    }
+
+    // Query method to CRUD INSERT/SELECT/UPDATE/DELETE
+
+    function viewdbtest() {
+        $this->load->model('dbtest_model');
+        $data['users'] = $this->dbtest_model->queryselect();
+
+        $this->load->view('crud/templates/header');
         $this->load->view('crud/view', $data);
         $this->load->view('crud/templates/footer');
-	}
-	
-	function insertdbtest(){
-		$this->load->database();
-		$this->load->model('dbtest_model');
-		$fname = "Lis'a";
-		$fname = $this->db->escape($fname); // while using the escape remove the quote marks in model method sql
-		$lname = "Lopez";
-		$email = "lisalopez@example.com";
-		$gender = "female";
-		$age = "25";
-		$result = $this->dbtest_model->queryinsert($fname, $lname, $email, $gender, $age);
-		if($result){
-			echo "Record Inserted";
-		}else{
-			echo "Failed to Insert Record";
-		}
-	}
-	
-	function updatedbtest(){
-		$this->load->model('dbtest_model');
-		$fname = "Lisa2";
-		$lname = "Lopez2";
-		$email = "lisalopez2@example.com";
-		$gender = "female";
-		$age = "23";
-		$id = 17;
-		$result = $this->dbtest_model->queryupdate($id, $fname, $lname, $email, $gender, $age);
-		if($result){
-			echo "Record Updated";
-		}else{
-			echo "Failed to Update the Record";
-		}
-	}
-	
-	function deletedbtest(){
-		$this->load->model('dbtest_model');
-		$id = 18;
-		$result = $this->dbtest_model->querydelete($id);
-		if($result){
-			echo "Record deleted";
-		}else{
-			echo "Failed to delete the Record";
-		}
-	}
+    }
 
-	function viewtest(){
-		$this->load->model('dbtest_model');
-		$data['users'] = $this->dbtest_model->qbselect();
-		
-		$this->load->view('crud/templates/header');
+    function insertdbtest() {
+        $this->load->database();
+        $this->load->model('dbtest_model');
+        $fname = "Lis'a";
+        $fname = $this->db->escape($fname); // while using the escape remove the quote marks in model method sql
+        $lname = "Lopez";
+        $email = "lisalopez@example.com";
+        $gender = "female";
+        $age = "25";
+        $result = $this->dbtest_model->queryinsert($fname, $lname, $email, $gender, $age);
+        if ($result) {
+            echo "Record Inserted";
+        } else {
+            echo "Failed to Insert Record";
+        }
+    }
+
+    function updatedbtest() {
+        $this->load->model('dbtest_model');
+        $fname = "Lisa2";
+        $lname = "Lopez2";
+        $email = "lisalopez2@example.com";
+        $gender = "female";
+        $age = "23";
+        $id = 17;
+        $result = $this->dbtest_model->queryupdate($id, $fname, $lname, $email, $gender, $age);
+        if ($result) {
+            echo "Record Updated";
+        } else {
+            echo "Failed to Update the Record";
+        }
+    }
+
+    function deletedbtest() {
+        $this->load->model('dbtest_model');
+        $id = 18;
+        $result = $this->dbtest_model->querydelete($id);
+        if ($result) {
+            echo "Record deleted";
+        } else {
+            echo "Failed to delete the Record";
+        }
+    }
+
+    function viewtest() {
+        $this->load->model('dbtest_model');
+        $data['users'] = $this->dbtest_model->qbselect();
+
+        $this->load->view('crud/templates/header');
         $this->load->view('crud/view', $data);
         $this->load->view('crud/templates/footer');
-	}
-	
-	function inserttest(){
-		$this->load->database();
-		$this->load->model('dbtest_model');
-		$result = $this->dbtest_model->qbinsert();
-		if($result){
-			echo "Record Inserted";
-		}else{
-			echo "Failed to Insert Record";
-		}
-	}
+    }
+
+    function inserttest() {
+        $this->load->database();
+        $this->load->model('dbtest_model');
+        $result = $this->dbtest_model->qbinsert();
+        if ($result) {
+            echo "Record Inserted";
+        } else {
+            echo "Failed to Insert Record";
+        }
+    }
+    
+    function updatetest() {
+        $this->load->model('dbtest_model');
+        $id = 19;
+        $result = $this->dbtest_model->qbupdate($id);
+        if ($result) {
+            echo "Record Updated";
+        } else {
+            echo "Failed to Update the Record";
+        }
+    }
+
 }
-
--- application/models/Dbtest_model.php
+++ application/models/Dbtest_model.php
@@ -6,102 +6,118 @@
  * @author vivekvengala
  */
 class Dbtest_model extends CI_Model {
-	// Read Operation with Query Method
-	public function queryselect(){
-		// we should load the database (autoload)
-		$this->load->database();
-		$query = $this->db->query('SELECT * FROM crud');
-		return $query->result_array();
-		//$result = $query->result_array();
-		//return $result;
-	}
-	
-	// Insert Operation with Query Method
-	public function queryinsert($fname, $lname, $email, $gender, $age){
-		//
-		$this->load->database();
-		$query = $this->db->query("INSERT INTO `crud` (first_name, last_name, email_id, gender, age) VALUES ($fname, '$lname', '$email', '$gender', '$age')");
-		//$result = $query->affected_array();
-		//return $this->db->affected_rows();
-		if($query){
-			return true;
-		}else{
-			return false;
-		}
-	}
-	
-	// Update Operation with Query Method
-	public function queryupdate($id, $fname, $lname, $email, $gender, $age){
-		//UPDATE `crud` SET first_name='$fname', last_name='$lname', gender='$gender', age=$age, email_id='$email' WHERE id=$id
-		$this->load->database();
-		$query = $this->db->query("UPDATE `crud` SET first_name='$fname', last_name='$lname', gender='$gender', age=$age, email_id='$email' WHERE id=$id");
-		//$result = $query->affected_array();
-		//return $this->db->affected_rows();
-		if($query){
-			return true;
-		}else{
-			return false;
-		}
-	}
-	
-	// Delete Operation with Query Method
-	public function querydelete($id){
-		$this->load->database();
-		$query = $this->db->query("DELETE FROM `crud` WHERE id=$id");
-		//$result = $query->affected_array();
-		//return $this->db->affected_rows();
-		if($query){
-			return true;
-		}else{
-			return false;
-		}
-	}
 
-	public function qbselect(){
-		$this->load->database();
-		//$query = $this->db->get('crud', 5, 2); // limit, offset
-		//$query = $this->db->get_where('crud', array('gender' => 'male'), 5, 2); // permits you to add a where clause in the second parameter, instead of using db->where() function
-		
-		// $this->db->select('first_name, last_name, email_id, gender, age, id'); // specific column names
-		// $this->db->from('crud');
-		// //$this->db->join('users', 'users.id=crud.id'); // for joining multiple tables
-		// $query = $this->db->get();
+    // Read Operation with Query Method
+    public function queryselect() {
+        // we should load the database (autoload)
+        $this->load->database();
+        $query = $this->db->query('SELECT * FROM crud');
+        return $query->result_array();
+        //$result = $query->result_array();
+        //return $result;
+    }
 
-		//$this->db->where('gender', 'male');
-		//$this->db->where('first_name !=', 'vivek');
-		//$this->db->or_where('id >', 4);
-		//$query = $this->db->get('crud1');
+    // Insert Operation with Query Method
+    public function queryinsert($fname, $lname, $email, $gender, $age) {
+        //
+        $this->load->database();
+        $query = $this->db->query("INSERT INTO `crud` (first_name, last_name, email_id, gender, age) VALUES ($fname, '$lname', '$email', '$gender', '$age')");
+        //$result = $query->affected_array();
+        //return $this->db->affected_rows();
+        if ($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-		$where = "`gender` = 'male' AND `first_name` = 'Daniel'";
-		$this->db->where($where);
-		$query = $this->db->get('crud');
-		
-		return $query->result_array();
-	}
+    // Update Operation with Query Method
+    public function queryupdate($id, $fname, $lname, $email, $gender, $age) {
+        //UPDATE `crud` SET first_name='$fname', last_name='$lname', gender='$gender', age=$age, email_id='$email' WHERE id=$id
+        $this->load->database();
+        $query = $this->db->query("UPDATE `crud` SET first_name='$fname', last_name='$lname', gender='$gender', age=$age, email_id='$email' WHERE id=$id");
+        //$result = $query->affected_array();
+        //return $this->db->affected_rows();
+        if ($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public function qbinsert(){
-		$this->load->database();
-		$data = array(
-				'first_name' => 'Justine',
-				'last_name' => 'Andrews',
-				'gender' => 'male',
-				'age' => '31',
-				'email_id' => 'justine87@example.com'
-		);
-		$query = $this->db->insert('crud', $data);
+    // Delete Operation with Query Method
+    public function querydelete($id) {
+        $this->load->database();
+        $query = $this->db->query("DELETE FROM `crud` WHERE id=$id");
+        //$result = $query->affected_array();
+        //return $this->db->affected_rows();
+        if ($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-		if($query){
-			return true;
-		}else{
-			return false;
-		}
-	}
+    public function qbselect() {
+        $this->load->database();
+        //$query = $this->db->get('crud', 5, 2); // limit, offset
+        //$query = $this->db->get_where('crud', array('gender' => 'male'), 5, 2); // permits you to add a where clause in the second parameter, instead of using db->where() function
+        // $this->db->select('first_name, last_name, email_id, gender, age, id'); // specific column names
+        // $this->db->from('crud');
+        // //$this->db->join('users', 'users.id=crud.id'); // for joining multiple tables
+        // $query = $this->db->get();
+        //$this->db->where('gender', 'male');
+        //$this->db->where('first_name !=', 'vivek');
+        //$this->db->or_where('id >', 4);
+        //$query = $this->db->get('crud1');
 
-	public function qbupdate(){
-		$this->load->database();
-	}
+        $where = "`gender` = 'male' AND `first_name` = 'Daniel'";
+        $this->db->where($where);
+        $query = $this->db->get('crud');
 
-	public function qbdelete(){
-		$this->load->database();
-	}
+        return $query->result_array();
+    }
+
+    public function qbinsert() {
+        $this->load->database();
+        $data = array(
+            'first_name' => 'Justine',
+            'last_name' => 'Andrews',
+            'gender' => 'male',
+            'age' => '31',
+            'email_id' => 'justine87@example.com'
+        );
+        $query = $this->db->insert('crud', $data);
+
+        if ($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function qbupdate($id) {
+        $this->load->database();
+        $data = array(
+            'first_name' => 'Justine4',
+            'last_name' => 'Andrews4',
+            'gender' => 'male2',
+            'age' => '29',
+            'email_id' => 'justine872@example.com'
+        );
+        //$query = $this->db->update('crud', $data, "id => $id");
+        $this->db->where('id', $id);
+        $query = $this->db->update('crud', $data);
+        
+        if ($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function qbdelete() {
+        $this->load->database();
+    }
+
 }
